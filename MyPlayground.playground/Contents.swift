f0fd3bd1c1f4d4ff8a24d82cad2b07617647fdd3
Guillaume Courtet <guillaume.courtet@hotmail.fr>
2017-07-02 15:34:34 +0100

    Playground init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+class Point {
+    
+    /*Attributes*/
+    
+    /*TO DO : initialise at (0,0)*/
+    
+    /*TO DO : initialise with a given position */
+    
+    
+    /*TO DO : moves a point to a new position */
+    
+    
+    /*TO DO : moves a point x position by dx and y position by dy */
+    
+    
+    /*TO DO : returns the distance to another point given in parameters*/
+    
+    
+    /*TO DO : returns the middle with another point given in parameters*/
+    
+}
+
+
+/*TEST YOUR CODE*/
+
+//1: create a new point with coordinates (4,7)
+
+
+//2: move your point to location (14,7)
+
+
+//3: move your point by (-10,-7)
+
+
+//4: create a point with coordinates (0,0)
+
+
+//5: get the distance between your two points
+
+
+//6: get the middle of your two points
